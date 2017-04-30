cbc0ad529beddb2be0bf01216ed69d82bfdf7d81
kisphp <mariusbogdan83@gmail.com>
2017-04-30 23:04:49 +0200

    added files

---
+++ README.md
@@ -0,0 +1,6 @@
+https://www.codementor.io/nodejs/tutorial/unit-testing-nodejs-tdd-mocha-sinon
+
+https://www.npmjs.com/package/colors
+
+[![Build Status](https://travis-ci.org/kisphp/gulp-commander.svg?branch=master)](https://travis-ci.org/kisphp/gulp-commander)
+
---
+++ tests/commander-test.js
@@ -0,0 +1,26 @@
+let gc = require('../src/gulp-commander');
+'use strict';
+
+let chai = require('chai');
+let expect = chai.expect; // we are using the "expect" style of Chai
+
+describe('GulpCommander', function() {
+    it('addTask should add one element to tasks list and none to watch list', function() {
+        gc.addTask('task-1');
+        expect(gc.getTasks().length).to.equal(1);
+        expect(gc.getWatch().length).to.equal(0);
+    });
+
+    it('addWatch should add one element to watch list and none to task list', function () {
+        gc.addWatch('watch-1');
+        expect(gc.getTasks().length).to.equal(1);
+        expect(gc.getWatch().length).to.equal(1);
+    });
+
+    it('getList must return one task and one watch', function(){
+        let list = gc.getList();
+
+        expect(list.tasks[0]).to.equal('task-1');
+        expect(list.watch[0]).to.equal('watch-1');
+    });
+});
